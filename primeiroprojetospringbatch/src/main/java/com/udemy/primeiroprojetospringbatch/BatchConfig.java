900bf6b6cb2ec27d403fc63eaee46271f8754fb6
C1321589 Matheus Da Silva Nascimento <c1321589@interno.bb.com.br>
2022-07-10 15:00:22 -0300

    get parameter

--- primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
+++ primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
@@ -5,10 +5,13 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,20 +23,27 @@ public class BatchConfig {
 
 	@Autowired
 	StepBuilderFactory stepBuilderFactory;
-	
+
 	@Bean
 	public Job ImprimeOlaJob() {
-		return jobBuilderFactory.get("ImprimeOlaJob").start(ImprimeOlaStep()).build();
+		return jobBuilderFactory.get("ImprimeOlaJob").start(ImprimeOlaStep()).incrementer(new RunIdIncrementer())
+				.build();
 	}
-	
+
 	public Step ImprimeOlaStep() {
-		return stepBuilderFactory.get("ImprimeOlaStep").tasklet(new Tasklet() {
-			
+		return stepBuilderFactory.get("ImprimeOlaStep").tasklet(mprimeOlaTasklet(null)).build();
+	}
+
+	@Bean
+	@StepScope
+	public Tasklet mprimeOlaTasklet(@Value("#{jobParameters['nome']}") String string) {
+		return new Tasklet() {
+
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("Ola Mundo");
+				System.out.println(String.format("Ola %s", string));
 				return RepeatStatus.FINISHED;
 			}
-		}).build();
+		};
 	}
 }
