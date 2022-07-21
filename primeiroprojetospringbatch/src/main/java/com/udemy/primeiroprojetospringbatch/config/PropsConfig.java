467e611159f42ff58de8e6e904c58729ce707684
C1321589 Matheus Da Silva Nascimento <c1321589@interno.bb.com.br>
2022-07-20 22:46:27 -0300

    production environment

--- primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/config/PropsConfig.java
+++ primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/config/PropsConfig.java
@@ -1,8 +1,5 @@
 package com.udemy.primeiroprojetospringbatch.config;
 
-import java.io.File;
-import java.nio.file.Paths;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
